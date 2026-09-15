0e2f83eb667f8f427d9e1df46ca7c9dedc403dcb
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-15 10:31:28 -0700

    refactor: use the public session tree entry point

--- lua-source.nix
+++ lua-source.nix
@@ -8,6 +8,7 @@ pkgs.runCommand "sysinit-wezterm-lua" { } ''
   mkdir -p $out
   cp -r ${./lua}/. $out/
   ln -s ${session-tree}/plugin/session_tree $out/session_tree
+  ln -s ${session-tree}/plugin/init.lua $out/session_tree_plugin.lua
   ln -s ${smart-keys}/plugin/init.lua $out/smart_keys_plugin.lua
   ln -s ${spawn}/plugin/init.lua $out/spawn_plugin.lua
 ''
--- lua/sysinit/pkg/ui/switcher.lua
+++ lua/sysinit/pkg/ui/switcher.lua
@@ -1,1 +1,8 @@
-return require("session_tree.switcher")
+local M = {}
+function M.setup(config, workspace_manager, context)
+  local options = require("session_tree.options").get()
+  options.workspace_manager = workspace_manager
+  options.context = context
+  require("session_tree_plugin").apply_to_config(config, options)
+end
+return M
